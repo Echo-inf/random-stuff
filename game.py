43ad0a973379d943a826197e8ab391570aff4d0b
Echo <101923390+Echo-inf@users.noreply.github.com>
2023-08-31 10:38:30 +0800

    Update game.py

--- game.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to my game!\nYou woke up in a dark place. You realise that you're tangled in spider web. You see a pocket knife just in your reach on the ground. What do you do?")
-input("")
-print("You then grab the knife and struggle to get out of the spider web...")
-wait(2)
-print("You finally got out of the spider web after some time. You walk out of the dark place and see a campfire nearby. Do you approach it?\ny/n")
-ch1 = input("")
-if "y" in ch1 (
-  print("you")
-)
